1be2bdfc05a9231ac29a896220209a14a8d98a4e
shashi kant <skntjee@gmail.com>
2024-10-15 01:23:00 +0530

    added file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useApi } from './UseApi'
 
 function App() {
+   
   const [count, setCount] = useState(0)
+  const [product , {  isLoading, error, data }]  = new  useApi(5000)
+  const [allProduct , {  isLoading:allProductIsLoading, error:isLoadingErro, data:isLoadingData }]  = new  useApi(8000)
+
+  
+  useEffect(()=>{
+    
+    product('https://fakestoreapi.com/products/1')
+    allProduct('https://fakestoreapi.com/products')
+
+  }, [])
+   
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       
+       {
+        isLoading?"product..": JSON.stringify(data)
+       }
+       <br />
+
+
+       {
+        allProductIsLoading?"allProductIsLoading...": JSON.stringify(isLoadingData)
+       }
+
+
+
     </>
   )
 }
---
+++ src/Axios.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AxiosCmp() {
+  return (
+    <div>Axios</div>
+  )
+}
+
+export default AxiosCmp
