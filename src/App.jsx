58aaa51af069961bed473937c946a09501a36890
shashi kant <skntjee@gmail.com>
2024-10-18 22:33:31 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useApi } from './UseApi'
+import React from 'react'
 
 function App() {
-   
-  const [count, setCount] = useState(0)
-  const [product , {  isLoading, error, data }]  = new  useApi(5000)
-  const [allProduct , {  isLoading:allProductIsLoading, error:isLoadingErro, data:isLoadingData }]  = new  useApi(8000)
-
-  
-  useEffect(()=>{
-    
-    product('https://fakestoreapi.com/products/1')
-    allProduct('https://fakestoreapi.com/products')
-
-  }, [])
-   
-
   return (
-    <>
-       
-       {
-        isLoading?"product..": JSON.stringify(data)
-       }
-       <br />
-
-
-       {
-        allProductIsLoading?"allProductIsLoading...": JSON.stringify(isLoadingData)
-       }
-
-
-
-    </>
+    <div>App</div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import AxiosCmp from './Axios.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-     <AxiosCmp />
+  
   </StrictMode>,
 )
